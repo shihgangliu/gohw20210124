4da4350e546014dfde21f149c5e1e83ad355065d
LeonLiu <shihgangliu@gmail.com>
2021-01-29 11:22:29 +0800

    Refactor and add one API
    - Refactor get a item API
    - Add delete a item API

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 
 	route.GET("/role", getRole)
 	route.GET("/role/:newid", getRole)
+	route.DELETE("/role/:newid", deleteRole)
 
 	route.Run(":8080")
 }
@@ -20,13 +21,39 @@ func getRole(c *gin.Context) {
 	newid := c.Param("newid")
 	if newid != "" {
 		if newid, err := strconv.Atoi(newid); err == nil {
-			c.JSON(200, Data[newid-1])
+			unewid := uint(newid)
+			for _, role := range Data {
+				if role.ID == unewid {
+					c.JSON(200, role)
+				}
+			}
 		} else {
-			c.String(400, "[Bad Request]: {{newid}}} is invalid")
+			c.String(400, "Item ID is invalid.")
 		}
 
 		return
 	}
 
 	c.JSON(200, Data)
+}
+
+func deleteRole(c *gin.Context) {
+	newid := c.Param("newid")
+	if newid, err := strconv.Atoi(newid); err == nil {
+		unewid := uint(newid)
+		for index, role := range Data {
+			if role.ID == unewid {
+				copy(Data[index:], Data[index+1:])
+				Data[len(Data)-1] = Role{}
+				Data = Data[:len(Data)-1]
+
+				c.String(200, "Item is deleted successfully.")
+				return
+			}
+		}
+
+		c.String(404, "Item is not found.")
+	} else {
+		c.String(400, "Item ID is invalid.")
+	}
 }
